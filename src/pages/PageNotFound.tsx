619811e3ad6f57c4a57291e7f159df6cdfb1fddb
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-05 18:47:17 +0545

    make text center

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -4,8 +4,8 @@ import { ChevronLeft } from 'lucide-react'
 
 const PageNotFound = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-6 items-center justify-center">
-      <h2 className="text-primary font-medium text-xl">The route you're trying to access could not be found.</h2>
+    <div className="min-h-screen flex flex-col gap-6 items-center justify-center text-center">
+      <h2 className="text-primary font-medium  text-xl">The route you're trying to access could not be found.</h2>
       <Button onClick={GoBack()}>Go Back <ChevronLeft /></Button>
     </div>
 
