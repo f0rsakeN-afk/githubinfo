9c9e9310c196bc324dc1425dbc91ca31beebfa24
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-05 18:35:17 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import UserInfo from "./pages/UserInfo"
+import PageNotFound from "./pages/PageNotFound"
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/:user" element={<UserInfo />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
