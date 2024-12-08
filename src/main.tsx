29ce32a8366ccc129d1ab960c9e785d5748e9e4a
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-08 16:32:30 +0545

    added themeprovider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
+import { ThemeProvider } from './context/themeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
       <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 )
